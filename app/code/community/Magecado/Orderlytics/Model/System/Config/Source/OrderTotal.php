b3a57b6fc1ec2f256e2be10517c32250700b08c7
Gil Greenberg <ggreenberg@somethingdigital.com>
2015-11-24 19:12:45 -0500

    Correctly output the total depending on choice selected

--- app/code/community/Magecado/Orderlytics/Model/System/Config/Source/OrderTotal.php
+++ app/code/community/Magecado/Orderlytics/Model/System/Config/Source/OrderTotal.php
@@ -4,9 +4,10 @@ class Magecado_Orderlytics_Model_System_Config_Source_OrderTotal
     public function toOptionArray()
     {
         return array(
-            array('value' => 'Grand Total', 'label' => Mage::helper('adminhtml')->__('Grand Total (Tax + Shipping)')),
-            array('value' => 'Subtotal-tax', 'label' => Mage::helper('adminhtml')->__('Sub total (Tax)')),
-            array('value' => 'Subtotal-ship', 'label' => Mage::helper('adminhtml')->__('Sub total (Shipping)')),
+            array('value' => 'grand_total', 'label' => Mage::helper('adminhtml')->__('Grand Total (Tax + Shipping)')),
+            array('value' => 'subtotal', 'label' => Mage::helper('adminhtml')->__('Sub total (No Tax or Shipping)')),
+            array('value' => 'subtotal_tax', 'label' => Mage::helper('adminhtml')->__('Sub total (Tax)')),
+            array('value' => 'Subtotal_ship', 'label' => Mage::helper('adminhtml')->__('Sub total (Shipping)')),
         );
     }
 }
